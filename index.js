8b50047d93c5699cc48bd22c8e3751b6f2eef210
MaxF1996 <maxeffect.apollo@gmail.com>
2023-05-23 23:28:06 +0300

    update

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 'use strict';
 
-const argv = require('yargs').argv;
 const contacts = require('./contacts');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
-      break;
+      const allContacts = contacts.listContacts();
+      return console.log(allContacts);
 
     case 'get':
-      // ... id
-      break;
+      const searchedContact = contacts.getContactById(id);
+      return console.log(searchedContact);
 
     case 'add':
-      // ... name email phone
-      break;
+      const newContact = contacts.addContact({ name, email, phone });
+      return console.log(newContact);
 
     case 'remove':
-      // ... id
-      break;
+      const removedContact = contacts.removeContact(id);
+      return console.log(removedContact);
 
     default:
       console.warn('\x1B[31m Unknown action type!');
