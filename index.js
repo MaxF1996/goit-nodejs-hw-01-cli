840f90cadbc3a230571f612fc3e74582a2bac2a7
MaxF1996 <maxeffect.apollo@gmail.com>
2023-05-23 22:42:29 +0300

    preparing some code

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const argv = require('yargs').argv;
+const contacts = require('./contacts');
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      // ...
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
